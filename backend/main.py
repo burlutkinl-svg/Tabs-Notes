71635e76cf1355eb8e28de4bb7bbfaaf558c2349
Lavr <burlutkinlavr@mail.ru>
2026-07-07 12:47:22 +0300

    Правки, добавил заглушку-шлюз для работы с дб

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,7 @@ from fastapi.middleware.cors import CORSMiddleware
 # Импорт роутеров
 from app.api import upload_photo
 from app.api import get_info
+from app.api import interact_with_db
 
 # Импорт схем
 from app.schemas import *
@@ -14,3 +15,4 @@ app = FastAPI()
 # Подключаем роутеры
 app.include_router(upload_photo.router, prefix="/api", tags=["upload_photo"])
 app.include_router(get_info.router, prefix="/api", tags=["create_user"])
+app.include_router(interact_with_db.router, prefix="/api", tags=["change_item_status"])
