f001c8a648c5953e427af59360c2058b0d5b63cc
Lavr <burlutkinlavr@mail.ru>
2026-07-07 12:47:22 +0300

    Правки, добавил заглушку-шлюз для работы с дб

--- backend/app/api/get_info.py
+++ backend/app/api/get_info.py
@@ -16,6 +16,8 @@ async def get_batches():
 
 @router.get("/get_batch_items")
 async def get_batch_items(id: int):
+    if id == -1:
+        return {"items": [{"batch_id": 1, "id": 1, "name":  "item1", "status": "completed"},{"batch_id": 1, "id": 2, "name":  "item2", "status": "pending"},{"batch_id": 1, "id": 3, "name":  "item3", "status": "proccessed"},{"batch_id": 1, "id": 4, "name":  "item4", "status": "failed"}]}
     return {"items": [{"id": 1, "name":  "item1", "status": "completed"},{"id": 2, "name":  "item2", "status": "pending"},{"id": 3, "name":  "item3", "status": "proccessed"},{"id": 4, "name":  "item4", "status": "failed"}]}
 
 @router.get("/download_batches")
